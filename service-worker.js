7a522559f0df525f558b06555aa6751651c25684
Craig Wayne <craigiswayne@gmail.com>
2025-03-02 12:34:16 +0000

    Increased lines

--- service-worker.js
+++ service-worker.js
@@ -3,9 +3,26 @@ function inject_quadrant_html(){
     if(document.querySelector(`.${class_name}`) !== null){
         return;
     }
-    const element = document.createElement('div');
-    element.classList.add(class_name);
-    document.body.appendChild(element);
+    const container = document.createElement('div');
+    container.classList.add(class_name);
+
+    const number_of_lines = 16;
+
+    for(let i=0; i<number_of_lines; i++){
+        const percent_number = 100/number_of_lines*i;
+        const percent = `${percent_number}%`;
+
+        const horizontal_line = container.appendChild(document.createElement('div'));
+        horizontal_line.style.setProperty('--percent', percent);
+        horizontal_line.setAttribute('title',percent);
+        horizontal_line.className = 'horizontal line';
+
+        const vertical_line = container.appendChild(document.createElement('div'));
+        vertical_line.style.setProperty('--percent', percent);
+        vertical_line.setAttribute('title',percent);
+        vertical_line.className = 'vertical line';
+    }
+    document.body.appendChild(container);
 }
 
 const ignored_urls = [
@@ -16,7 +33,8 @@ const ignored_urls = [
 
 chrome.action.onClicked.addListener(async (tab) => {
 
-    const current_url_is_ignored = ignored_urls.filter(ignored_url => tab.url.includes(ignored_url)).length !== 0;
+    const current_url_is_ignored = ignored_urls
+        .filter(ignored_url => tab.url.includes(ignored_url)).length !== 0;
 
     if(current_url_is_ignored){
         return;
